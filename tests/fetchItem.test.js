6f8b93f82aea9f1bc1581ed377379ff337dd24a7
JohnnyNWT <jooaaooskt@gmail.com>
2022-08-12 16:41:12 -0300

    Req - 3

--- tests/fetchItem.test.js
+++ tests/fetchItem.test.js
@@ -6,4 +6,24 @@ describe('2 - Teste a função fetchItem', () => {
   it('2.1 - Testa se fetchItem é uma função', () => {
     expect(typeof fetchItem).toBe('function');
   });
+
+  it('2.2 - Verifica se a função fetch foi chamada', async () => {
+    await fetchItem('MLB1615760527');
+    expect(fetch).toHaveBeenCalled();
+  });
+
+  it('2.3 - Verifica se a função ultiliza o ENDPOINT correto', async () => {
+    await fetchItem('MLB16157605');
+    const ENDPOINT = 'https://api.mercadolibre.com/items/MLB1615760527';
+    expect(fetch).toHaveBeenCalledWith(ENDPOINT);
+  });
+
+  it('2.4 - Testa se o retorno da função é uma estrutura de dados igual ao objeto item', async () => {
+    const funcao = await fetchItem('MLB1615760527');
+    expect(funcao).toEqual(item);
+  });
+
+  it('2.5 - Testa se a função retorna um erro ao chamar sem argumento', () => {
+    expect(fetchItem()).rejects.toThow(new Error('You must provide an url'));
+  });
 });
